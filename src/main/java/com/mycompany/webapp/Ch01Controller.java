d6e4b9899c1a02735a8944692096298953841e84
jungwoo <kjw970103@gmail.com>
2021-08-26 16:35:35 +0900

    최초 프로젝트 공유 파일

---
+++ src/main/java/com/mycompany/webapp/Ch01Controller.java
@@ -0,0 +1,19 @@
+package com.mycompany.webapp;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/ch01")
+public class Ch01Controller {
+	private static final Logger logger = LoggerFactory.getLogger(Ch01Controller.class);
+	
+	@RequestMapping("/content")
+	public String home() {
+		logger.debug("실행1");
+		return "ch01/content";
+	}
+}
